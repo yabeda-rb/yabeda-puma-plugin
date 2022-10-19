bd0974f4ab8d0cb1aab1e8da324a82499aa2b5ae
Andrey Novikov <envek@envek.name>
2022-10-19 22:10:48 +0900

    Revert "require Puma 6"
    This reverts commit de877a60168ddf8fd615ffd17485c2c848e5e445.

--- lib/puma/plugin/yabeda_prometheus.rb
+++ lib/puma/plugin/yabeda_prometheus.rb
@@ -32,7 +32,16 @@ Puma::Plugin.create do
 
       metrics = Puma::Server.new app, events, min_threads: 0, max_threads: 1
 
-      metrics.log_writer.log "* Starting Yabeda Prometheus metrics exporter on http://#{host}:#{port}#{path}"
+      if metrics.respond_to?(:min_threads=)
+        metrics.min_threads = 0
+        metrics.max_threads = 1
+      end
+
+      if metrics.respond_to?(:log_writer)
+        metrics.log_writer.log "* Starting Yabeda Prometheus metrics exporter on http://#{host}:#{port}#{path}"
+      else
+        events.log "* Starting Yabeda Prometheus metrics exporter on http://#{host}:#{port}#{path}"
+      end
 
       metrics.add_tcp_listener host, port
 
